280059c5dff7f75d1621a9cfc1ebad95ba648993
LazyDope <mydj555@gmail.com>
2023-07-17 11:17:33 -0400

    fix(row): return result
    (cherry picked from commit 6c8d96b5edfa97a0960b7d0ec470e302c84ea849)
    (cherry picked from commit 9e65ac9d5fbc3b6d17a58b64b95d04c4a63e9a97)

--- src/row.rs
+++ src/row.rs
@@ -415,7 +415,7 @@ impl Row {
 
     /// Retrieve a column's data for a given column index.
     #[track_caller]
-    pub fn get_column_data<'a, I>(&'a self, idx: I) -> crate::Result<ColumnData<'static>>
+    pub fn get_column_data<'a, I>(&'a self, idx: I) -> crate::Result<&'a ColumnData<'static>>
     where
         I: QueryIdx,
     {
@@ -423,7 +423,7 @@ impl Row {
             Error::Conversion(format!("Could not find column with index {}", idx).into())
         })?;
 
-        self.data.get(idx).unwrap()
+        Ok(self.data.get(idx).unwrap())
     }
 }
 
